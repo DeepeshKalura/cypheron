35817ac6dd3427d60bf590d2b346f6799ea1ee50
DeepeshKalura <deepeshkalurs@gmail.com>
2025-11-22 11:55:38 +0530

    feat : added logo thanks man!!

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,15 +17,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
+        url: "/logo.svg",
         type: "image/svg+xml",
       },
     ],
